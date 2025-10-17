508ed556cd8023a4da58c34d979b40281118184c
divyansh singh <divyanshsingh@divyanshs-MacBook-Air.local>
2025-10-17 18:48:52 +0530

    added the api-error

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -1,11 +1,22 @@
 class ApiError extends Error {
-    constructor(statusCode, message = "Something went wrong", errors = [], stack = "") {
+    constructor(statusCode,
+         message = "Something went wrong",
+          errors = [], 
+          stack = ""
+        )
+    {
         super(message);
         this.statusCode = statusCode;
+        this.data = null;
         this.message = message;
         this.errors = errors;
-        if (stack) this.stack = stack;
-    }
+        if (stack) this.stack = stack
+          else {
+            Error (captureStacktrace(   this , this.constructor ))
+          };
+    } 
 }
 
-module.exports = ApiError;
+export {ApiError};
+// copy paste karne ka 
+
